f457c9473132ed6b472089faed8a34b3f91b0aca
Cho Hwijung <111957032+Saryrndeth@users.noreply.github.com>
2024-06-12 00:01:49 +0900

    Add files via upload

--- execute.py
+++ execute.py
@@ -2,7 +2,6 @@ import sys
 
 
 L = sys.argv[1:]
-# L.append("for i in range(10):\n\tprint(i)")
 for i in range(len(L)):
     L[i] = L[i].replace("_", " ").replace('\\n', '\n').replace('\\t', '\t')
 for i in L:
